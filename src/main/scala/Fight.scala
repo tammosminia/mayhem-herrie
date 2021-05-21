4be707c8fd34c31e2a2e2df6f59ef2ecbee17c2b
tammosminia <tammosminia@gmail.com>
2021-05-21 15:13:23 +0200

    mayhem

--- src/main/scala/Fight.scala
+++ src/main/scala/Fight.scala
@@ -8,20 +8,25 @@ import scala.util.Random
 object Fight {
   def randomFromList[T](l: List[T]): Option[T] = if (l.isEmpty) None else Some(l(Random.nextInt(l.length)))
 
+  def chooseSkill(h: Hero, sm: StatusMessage): Option[Skill] = {
+    val allowed = h.skills.filter(_.cooldown == 0).filter(_.power + h.power >= 0)
+    randomFromList(allowed)
+  }
+
   def chooseTarget(h: Hero, s: Skill, sm: StatusMessage): Option[Hero] =
     s.allowedTarget match {
       case AllowedTarget.self                    => Some(h)
-      case AllowedTarget.others if s.isPleasant  => randomFromList(sm.you.filter(_ != h))
-      case AllowedTarget.others if !s.isPleasant => randomFromList(sm.opponents)
-      case AllowedTarget.all if s.isPleasant     => randomFromList(sm.you)
-      case AllowedTarget.all if !s.isPleasant    => randomFromList(sm.opponents)
+      case AllowedTarget.others if s.isPleasant  => randomFromList(sm.you.filter(_ != h).filter(_.isAlive))
+      case AllowedTarget.others if !s.isPleasant => randomFromList(sm.opponents.filter(_.isAlive))
+      case AllowedTarget.all if s.isPleasant     => randomFromList(sm.you.filter(_.isAlive))
+      case AllowedTarget.all if !s.isPleasant    => randomFromList(sm.opponents.filter(_.isAlive))
     }
 
   def actionForHero(h: Hero, sm: StatusMessage): Option[ActionMessage] = {
     if (!h.isAlive || h.isBusy) None
     else {
       for {
-        skill <- randomFromList(h.skills.filter(_.cooldown > 0))
+        skill <- chooseSkill(h, sm)
         target <- chooseTarget(h, skill, sm)
       } yield ActionMessage(h.id, skill.id, target.id, false)
     }
